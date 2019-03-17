37a30039eca9fba6d4ab364b52c3078386a85416
Raman Khilko <torino_cat@Ramans-MacBook-Pro.local>
2019-03-17 15:33:30 +0300

    Create base project

---
+++ Ziro_iOS/Ziro_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ziro_iOS
+//
+//  Created by Raman Khilko on 3/17/19.
+//  Copyright © 2019 Raman Khilko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
